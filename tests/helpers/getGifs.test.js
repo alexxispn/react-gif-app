4ad97c6c17347f230246a1602068b143fd7d096b
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-11-25 11:54:13 +0000

    add tests to getGifs

--- tests/helpers/getGifs.test.js
+++ tests/helpers/getGifs.test.js
@@ -1,6 +1,15 @@
-import {describe, it, expect} from "vitest";
-describe('AddCategory', () => {
-    it('should show correctly', () => {
-        expect(true).toBe(true);
-    })
+import {describe, expect, it} from "vitest";
+import {getGifs} from "../../src/helpers/getGifs";
+
+describe('getGifs Fetch', () => {
+    it('should return an array of objects', async () => {
+        const gif = {
+            id: expect.any(String),
+            title: expect.any(String),
+            url: expect.any(String)
+        };
+        const gifs = await getGifs('One Punch');
+        expect(gifs.length).toBe(10);
+        expect(gifs).toEqual(expect.arrayContaining([gif]));
+    });
 })
